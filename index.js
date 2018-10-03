6495a81e7d44fdef70572e56cfb1455e2f1bb566
jeon0123 <43804613+jeon0123@users.noreply.github.com>
2018-10-03 22:06:17 +0900

    Update index.js

--- index.js
+++ index.js
@@ -44,50 +44,18 @@ console.log('Answer: ' + obj['경주봉황대']['정의']); //// debugging
 app.post('/', function (request, response) {
     console.log('request: \n' + JSON.stringify(request.body));
     //var item = req.body.result.parameters['item'];
-    var what = request.body.queryResult.parameters['what'];
-    var who = request.body.queryResult.parameters['who'];
-    var when = request.body.queryResult.parameters['when'];
-    var where = request.body.queryResult.parameters['where'];
-    var how = request.body.queryResult.parameters['how'];
-    var why = request.body.queryResult.parameters['why'];
-
+    var entities_1 = request.body.queryResult.parameters['entities1'];
+    var entities_2 = request.body.queryResult.parameters['entities2'];
+    var entities_3 = request.body.queryResult.parameters['entities3'];
+    
     let action = (request.body.queryResult.action) ? request.body.queryResult.action: 'default';
 
 
     const actionHandlers = {
-        'what.how': () => {
-            let responseToUser = { fulfillmentText: obj[what][how]};
+        'entities_1.entities_2.entities_3': () => {
+            let responseToUser = { fulfillmentText: obj[entities1][entities2][entities3]};
             sendResponse(responseToUser);
         },
-
-        'what.how.why': () => {
-            let responseToUser = { fulfillmentText: obj[what][how][why]};
-            sendResponse(responseToUser);
-        },
-
-        'what.when.how': () => {
-            let responseToUser = { fulfillmentText: obj[what][when][how]};
-            sendResponse(responseToUser);
-        },
-        
-                
-        'what.where.how': () => {
-            let responseToUser = { fulfillmentText: obj[what][where][how]};
-            sendResponse(responseToUser);
-        },
-        
-        
-        'what.who': () => {
-            let responseToUser = { fulfillmentText: obj[what][who]};
-            sendResponse(responseToUser);
-        },
-       
-        'what.who.how': () => {
-            let responseToUser = { fulfillmentText: obj[what][who][how]};
-            sendResponse(responseToUser);
-        },
-
-          
         
         'default': () => {
             let responseToUser = { fulfillmentText: '정보가 없는 내용입니다.' };
@@ -119,4 +87,3 @@ app.post('/', function (request, response) {
         }
     }
 });
-
