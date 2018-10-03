8a19ce0cddd3997288589f3b011bdbd42abea4f1
jeon0123 <43804613+jeon0123@users.noreply.github.com>
2018-10-03 22:10:00 +0900

    Update index.js

--- index.js
+++ index.js
@@ -20,7 +20,7 @@ app.use(bodyParser.urlencoded({ extended: true })); //
 var fs = require('fs');
 var obj = JSON.parse(fs.readFileSync('info.json', 'utf8'));
 
-console.log('Answer: ' + obj['경주봉황대']['정의']); //// debugging
+console.log('Answer: ' + obj['강아지']['아침']['집']); //// debugging
 
 // app.post('/', function(req, res){
 //   var speech = 
